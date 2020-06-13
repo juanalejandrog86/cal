3bc099061a16a8e65a410e1dfa832596410ea2cd
Juan Gomez <juan@debian.yaguas.com>
2020-06-13 14:43:37 -0500

    Se agrega version de java para compilacion y algunas lineas en el main

--- src/main/java/com/jagp/app/App.java
+++ src/main/java/com/jagp/app/App.java
@@ -12,6 +12,14 @@ public class App
        java.lang.String cadena = new String();
        java.lang.Double numero = new Double(1);
        
-       System.out.println( "Hello World!" );
+       System.out.println( "this is a sample test" );
+       newText();
+    }
+
+    private static String newText(){
+	for(int i=20; i<40;i++){    
+    	        System.out.println("Totalmente mucho mas rápido de hacer");
+	}
+	return null;
     }
 }
